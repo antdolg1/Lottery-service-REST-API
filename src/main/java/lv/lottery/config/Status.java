12939d87021ec1d8c78d73c0a3d8ea3ee2f71a76
Antons.Dolgopolovs <antdolg@gmail.com>
2020-02-03 14:24:59 +0200

    Status issue fixed

---
+++ src/main/java/lv/lottery/config/Status.java
@@ -0,0 +1,9 @@
+package lv.lottery.config;
+
+public class Status {
+    public static final String RESPONSE_OK = "OK";
+    public static final String RESPONSE_FAIL = "FAIL";
+    public static final String LOTTERY_OPEN = "OPEN";
+    public static final String LOTTERY_CLOSED = "CLOSED";
+    public static final String WINNER_SELECTED = "WINNER SELECTED";
+}
---
+++ src/main/java/lv/lottery/dto/StopRegistrationDTO.java
@@ -0,0 +1,17 @@
+package lv.lottery.dto;
+
+public class StopRegistrationDTO {
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public StopRegistrationDTO(Long id) {
+        this.id = id;
+    }
+}
