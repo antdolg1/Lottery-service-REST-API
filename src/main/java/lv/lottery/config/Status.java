0e109cb85f472702c7c609bcd4665e40eef98176
Antons.Dolgopolovs <antdolg@gmail.com>
2020-02-07 21:25:56 +0200

    Status class updated

--- src/main/java/lv/lottery/config/Status.java
+++ src/main/java/lv/lottery/config/Status.java
@@ -6,4 +6,7 @@ public class Status {
     public static final String LOTTERY_OPEN = "OPEN";
     public static final String LOTTERY_CLOSED = "CLOSED";
     public static final String WINNER_SELECTED = "WINNER SELECTED";
+    public static final String STATUS_WIN = "WIN";
+    public static final String STATUS_LOSE = "LOSE";
+    public static final String STATUS_PENDING = "PENDING";
 }
