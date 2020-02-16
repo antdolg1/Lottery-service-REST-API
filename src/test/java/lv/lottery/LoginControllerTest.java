9383ee4999820eb95e12e240c45b2a8be805e10d
Antons.Dolgopolovs <antdolg@gmail.com>
2020-02-16 16:36:20 +0200

    LoginControllerTest added

---
+++ src/test/java/lv/lottery/LoginControllerTest.java
@@ -0,0 +1,32 @@
+package lv.lottery;
+
+import static
+        org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static
+        org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static
+        org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import lv.lottery.controller.LoginController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(LoginController.class)
+public class LoginControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void testLoginPage() throws Exception {
+        mockMvc.perform(get("/login"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("login"));
+    }
+}
